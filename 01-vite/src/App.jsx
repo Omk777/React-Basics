f3dc7b78d1ccfde1d19194b86a81d7afce196a50
Your Name <you@example.com>
2025-10-29 11:29:51 +0530

    first react

---
+++ 01-vite/src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import './App.css'
+
+function App() {
+
+const name='Omkar';
+  return (
+
+    <>
+    <Header></Header>
+    <h1>Hello {name}</h1><p>Welcome to react course</p>
+    <h2>React Crash Course</h2>
+    </>
+  )
+}
+
+function Header(){
+  return (
+    <h2>Header Section</h2>
+  )
+}
+
+
+export default App
