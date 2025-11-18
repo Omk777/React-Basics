82b30ddcc7863c092a78c6cd7ef8c8b4574628a0
Your Name <you@example.com>
2025-11-18 11:17:37 +0530

    search-changes

--- React-final/src/components/UI/SearchFilter.jsx
+++ React-final/src/components/UI/SearchFilter.jsx
@@ -14,7 +14,7 @@ function SearchFilter({search,setSearch,filter,setFilter}) {
   return (
     <div>
         <section className='section-searchFilter container '>
-            <input type="text"  placeholder='search' value={search} onChange={handleInputChange}/>
+            <input type="text"  placeholder='search' value={search} onChange={handleInputChange} className="w-full px-4 py-2 rounded-xl bg-white/20 backdrop-blur-md border border-white/30 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-300"/>
             <div>
                 <select className="select-section" value={filter} onChange={handleSelectChange}>
                 <option value="all">All</option>
